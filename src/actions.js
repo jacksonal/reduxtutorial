76447ccbe1e74e1d96fb4d0cb980ad999f1ffd19
Alex Jackson <alex.lee.jackson@gmail.com>
2016-08-13 14:50:17 -0400

    fetch posts if needed

--- src/actions.js
+++ src/actions.js
@@ -37,7 +37,7 @@ export function receivePosts(subreddit, json){
 // Meet our first thunk action creator!
 // Though its insides are different, you would use it just like any other action creator:
 // store.dispatch(fetchPosts('reactjs'))
-export function fetchPosts(subreddit){
+function fetchPosts(subreddit){
 	// Thunk middleware knows how to handle functions.
   // It passes the dispatch method as an argument to the function,
   // thus making it able to dispatch actions itself.
@@ -66,3 +66,30 @@ export function fetchPosts(subreddit){
 	}
 }
 
+function shouldFetchPosts(state, subreddit){
+	const posts = state.postsBySubreddit[subreddit];
+	if(!posts){
+		return true;
+	} else if(posts.isFetching){
+		return false;
+	} else{
+		return posts.didInvalidate;
+	}
+}
+
+export function fetchPostsIfNeeded(subreddit){
+	// Note that the function also receives getState()
+  // which lets you choose what to dispatch next.
+
+  // This is useful for avoiding a network request if
+  // a cached value is already available.
+  return (dispatch, getState) => {
+	  if(shouldFetchPosts(getState(), subreddit)) {
+		  // Dispatch a thunk from thunk!
+		  return dispatch(fetchPosts(subreddit));
+	  } else{
+		  // Let the calling code know there's nothing to wait for.
+		return Promise.resolve()
+	  }
+  }
+}
--- src/index.js
+++ src/index.js
@@ -16,6 +16,6 @@ const store = createStore(
 )
 
 store.dispatch(selectSubreddit('reactjs'))
-store.dispatch(fetchPosts('reactjs')).then(() =>
+store.dispatch(fetchPostsIfNeeded('reactjs')).then(() =>
   console.log(store.getState())
 )
