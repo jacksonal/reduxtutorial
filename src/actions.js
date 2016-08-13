aa19d301501283f113005621a2bd0d9b0fe5aa87
Alex Jackson <alex.lee.jackson@gmail.com>
2016-08-13 14:39:58 -0400

    adding thunk middleware and async function to fetch

--- src/actions.js
+++ src/actions.js
@@ -1,3 +1,4 @@
+import fetch from 'isomorphic-fetch';
 export const SELECT_SUBREDDIT = 'SELECT_SUBREDDIT';
 export const INVALIDATE_SUBREDDIT = 'INVALIDATE_SUBREDDIT';
 export const REQUEST_POSTS = 'REQUEST_POSTS';
@@ -32,3 +33,36 @@ export function receivePosts(subreddit, json){
 		receivedAt: Date.now()
 	}
 }
+
+// Meet our first thunk action creator!
+// Though its insides are different, you would use it just like any other action creator:
+// store.dispatch(fetchPosts('reactjs'))
+export function fetchPosts(subreddit){
+	// Thunk middleware knows how to handle functions.
+  // It passes the dispatch method as an argument to the function,
+  // thus making it able to dispatch actions itself.
+	return function(dispatch){
+		// First dispatch: the app state is updated to inform
+    // that the API call is starting.
+		
+		dispatch(requestPosts(subreddit))
+		
+		// The function called by the thunk middleware can return a value,
+    // that is passed on as the return value of the dispatch method.
+
+    // In this case, we return a promise to wait for.
+    // This is not required by thunk middleware, but it is convenient for us.
+
+		return fetch('http://ww.reddit.com/r/${subreddit}.json')
+			.then(response => response.json())
+			.then(json => 
+				// We can dispatch many times!
+        // Here, we update the app state with the results of the API call.
+
+				dispatch(receivePosts(subreddit, json))
+			)
+		// In a real world app, you also want to
+      // catch any error in the network call.
+	}
+}
+
